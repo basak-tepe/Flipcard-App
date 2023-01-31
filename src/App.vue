65c22986b62c0eeaa1415089c7d9a030a0f44a36
basak-tepe <basaktepe2020@gmail.com>
2023-01-31 23:52:43 +0300

    Added storage component (Pinia)

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,38 @@
 <template>
   <div>
     <div class="links">
-      <router-link class="nav-link" active-class="active"  to="/">
+      <router-link class="nav-link" active-class="active"  to="/"  :to="{ name: 'Home', params: { flashCards: flashCards } }" >
         Home Page
       </router-link>
 
-      <router-link class="nav-link" active-class="active" to="/game">
-        Game
-      </router-link>
+      <router-link class="nav-link" active-class="active" to="/game" :to="{ name: 'Game', params: { flashCards: flashCards } }">
+      Game
+    </router-link>
+
     </div>
-    <router-view></router-view>
-
-    <InputPart :count="count" :flashCards="flashCards" @update:count="updateCount"
-      @update:flashcards="updateFlashCards" />
-
-
-    <ul id="flipCards" class="grid list-none">
-      <li v-for="flipcard in flashCards" class="col-12 sm:col-12 md:col-6 lg:col-3">
-        <div class="border-round-md shadow-6">
-          <FlipCard :flipcard="flipcard" @delete-card="deleteCard" />
-        </div>
-      </li>
-    </ul>
+    <router-view  @flashCards-updated="updateFlashCards"></router-view>
 
   </div>
 </template>
 
 
 <script>
-import InputPart from './components/InputPart.vue'
-import FlipCard from './components/FlipCard.vue'
-
-export default {
-
-  data() {
+export default{
+  
+  data (){
     return {
       count: 0,
-      //flashcards is a 2D list with cards inside [["front","back"],[],...]
       flashCards: [],
-
     }
   },
 
   methods: {
-    updateCount(count) {
-      this.count = count;
-      console.log(this.count);
-    },
-
-    updateFlashCards(cardInfo) {
-      this.flashCards.push(cardInfo);
-    },
-
-    deleteCard(deleted) {
-      this.flashCards = this.flashCards.filter(function (card) {
-        return card !== deleted;
-      });
-      this.count = this.flashCards.length;
+    updateFlashCards (updatedFlashCards) {
+      this.flashCards = updatedFlashCards;
     }
-  },
-
-  components: {
-    InputPart,
-    FlipCard,
   }
 }
+
 </script>
 
-
-<style>
-
-.links{
-  justify-content: right;
-  display: flex;
-}
-
-.nav-link {
-  margin: 10px;
-  padding: 5px 10px;
-  background-color: beige;
-  text-decoration: none;
-  color: black;
-  border-radius: 5px;
-  font-size: 15px;
-}
-
-.active {
-  margin: 10px;
-  padding: 5px 10px;
-  background-color: lightblue;
-  text-decoration: none;
-  color: navy;
-  border-radius: 5px;
-  font-size: 15px;
-} 
-
-.flipCards {
-  align-items: center;
-  justify-content: center;
-  display: flex;
-}
-</style>
+<style></style>
